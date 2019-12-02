44c06d3a2d9822c64a9e8fc37b6e05307a410c1a
mikebeau <mikebeau@bu.edu>
2019-12-02 17:39:06 -0500

    Added buttons, and second page

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button openInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openInfo = (Button) findViewById(R.id.openInfo);
+        openInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityInformation();
+            }
+        });
+    }
+    public void openActivityInformation(){
+        Intent intent = new Intent(this, Information.class);
+        startActivity(intent);
     }
 }
