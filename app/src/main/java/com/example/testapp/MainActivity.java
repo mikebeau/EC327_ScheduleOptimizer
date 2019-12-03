59c61765caf1b8f81f3dc9ebe0f4b24cc3b777fc
mikebeau <mikebeau@bu.edu>
2019-12-02 19:10:38 -0500

    added saved schedules page

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -10,11 +10,20 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button openInfo;
+    private Button openSavedSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openSavedSchedule = (Button) findViewById(R.id.openSavedSchedule);
+        openSavedSchedule.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivitySavedSchedules();
+            }
+        });
 
         openInfo = (Button) findViewById(R.id.openInfo);
         openInfo.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openActivityInformation(){
         Intent intent = new Intent(this, Information.class);
+        startActivity(intent);
+    }
+
+    public void openActivitySavedSchedules(){
+        Intent intent = new Intent(this, SavedSchedules.class);
         startActivity(intent);
     }
 }
